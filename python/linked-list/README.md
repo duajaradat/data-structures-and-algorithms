c56f7ae361b1411f6d852430995d19c835688ae2
duajaradat <duajaradat164@gmail.com>
2021-10-17 00:54:44 +0300

    whiteboard , readme ,coding

--- python/linked-list/README.md
+++ python/linked-list/README.md
@@ -1,11 +1,58 @@
-### Solar System Exploration, 1950s – 1960s
+# Implementation: Singly Linked Lists
 
-- [ ] Mercury
-- [x] Venus
-- [x] Earth (Orbit/Moon)
-- [x] Mars
-- [ ] Jupiter
-- [ ] Saturn
-- [ ] Uranus
-- [ ] Neptune
-- [ ] Comet Haley
+
+*Author: Du'a Jaradat*
+
+---
+
+
+
+### Problem Domain
+******
+
+Create a Node class that has properties for the
+value stored in the Node, and a pointer to the next Node
+
+---
+
+
+
+### Whiteboard Visual
+***[Insert method]***
+
+***[class Node]***
+
+![class-str-linked-list](class-str-linked-list.png)
+
+![Insert-linked-list](Insert-linked-list.png)
+
+***[Includes method]***
+
+![Includes-linked-list](Includes-linked-list.png)
+---
+
+
+For more information on Markdown: https://www.markdownguide.org/cheat-sheet
+
+---
+
+
+### Big O
+
+| method| Time | Space |
+|------ | :----------- | :----------- |
+| insert | O(1) |O(1) |
+| includes | O(n) |O(1) |
+
+---
+
+### Structure and Testing
+
+- [x] Can successfully instantiate an empty linked list
+- [x] Can properly insert into the linked list
+- [x] The head property will properly point to the first node in the linked list
+- [x] Can properly insert multiple nodes into the linked list
+- [x] Will return true when finding a value within the linked list that exists
+- [x] Will return false when searching for a value in the linked list taht dose not exists
+- [x] Can properly return a collection of all the values that exist in the linked list
+
---
+++ python/linked-list/linked_list.py
@@ -0,0 +1,72 @@
+class Node:
+    """
+    Node class creates instances that has attributes value stored in the node and a pointer to next node
+    """
+    def __init__(self,value,next=None):
+        self.value = value
+        self.next = next
+
+class LinkedList:
+    """
+    class linked list is sequence of nodes that are connected to each other with pointer to next node
+    """
+    # at intialization an empty Linked List
+    def __init__(self):
+        self.head=None
+
+    def insert(self, value):
+        """
+        insert : create a node to insert new_node at the beginning of the list
+        Arguments: value
+        Returns: nothing
+        Adds a new node with that value to the head of the list with an O(1) Time performance.
+        """
+        # create a new node from Node class with value
+        new_node=Node(value)
+
+        # if the linked list is empty, add new_node to the head , if not empty then set the poiter of the new_node to the head and set the new_node to the head
+        if not self.head:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+    def includes(self,value):
+        """
+        includes : to check if the list contains a node contains the value and return True if yes and False if no
+        Arguments: value
+        Returns: Boolean
+        Indicates whether that value exists as a Node’s value somewhere within the list.
+        """
+
+        if not self.head:
+            return False
+        else:
+            current_Node = self.head # to search for the value , put the pointer at the head to start the search
+            while current_Node != None: # loop in list unitl the current_Node is None
+                if current_Node.value == value:
+                    return True
+                else:
+                    current_Node = current_Node.next
+            return False
+
+    def __str__(self):
+        """
+        to string : fun with no arguments , used to print a string with all the values in the linked list
+
+        Arguments: none
+        Returns: a string representing all the values in the Linked List, formatted as:
+        "{ a } -> { b } -> { c } -> NULL"
+        """
+        current_Node=self.head
+        print(current_Node)
+        linked_list_values=''
+        while current_Node !=None:
+            linked_list_values+=f"{current_Node} -> "
+            current_Node = current_Node.next
+        linked_list_values+="NULL"
+        return linked_list_values
+
+
+
+
+
