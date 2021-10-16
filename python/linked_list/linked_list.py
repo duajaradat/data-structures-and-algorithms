4d2d4c41265c8c9b9e826c4fd4b73690149b3b00
duajaradat <duajaradat164@gmail.com>
2021-10-16 22:44:25 +0300

    edit file ll

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,28 +1,12 @@
-class Node:
+class LinkedList:
     """
     Put docstring here
     """
 
-    def __init__(self,value=None,next=None):
-        """
-        Node class has a value that stored the Node and pointer to the next node
-        """
-        self.value = value
-        self.next = next
+    def __init__(self):
+        # initialization here
+        pass
 
-
-    class Linked_list():
-        """
-        Linked list include a head property.
-        Upon instantiation, an empty Linked List should be created
-        """
-        def __init__(self,node=None):
-            self.head = node
-
-        def insert(self,value):
-            node=Node(value)
-            self.head = node
-
-
-
+    def some_method(self):
+        # method body here
         pass
