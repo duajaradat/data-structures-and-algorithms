c7d2a836318c565f293e0a9b32d07ef96ee5dcae
duajaradat <duajaradat164@gmail.com>
2021-10-18 09:15:22 +0300

    edit the table of contents code-challenges

--- python/tests/data_structure/test_linked_list.py
+++ python/tests/data_structure/test_linked_list.py
@@ -13,11 +13,6 @@ def test_LinkedList_include_one():
     assert test_LinkedList.includes(1) == True
 
 
-def test_LinkedList_include_two():
-    test_LinkedList = LinkedList()
-    test_LinkedList.insert(0)
-    test_LinkedList.insert(1)
-    assert test_LinkedList.includes(100) == False
 
 
 
@@ -25,3 +20,21 @@ def test_LinkedList_insert_case_two():
     test_LinkedList = LinkedList()
     test_LinkedList.insert(0)
     assert test_LinkedList.head.value==0
+
+
+def test_LinkedList_include_two():
+    test_LinkedList = LinkedList()
+    test_LinkedList.insert(0)
+    test_LinkedList.insert(1)
+    assert test_LinkedList.includes(100) == False
+
+
+def test_LinkedList_insertbefore():
+    test_LinkedList=LinkedList()
+    test_LinkedList.insert(6)
+    test_LinkedList.insert(34)
+    test_LinkedList.insert(68)
+    test_LinkedList.append(9)
+    test_LinkedList.insert_after(34,1)
+    test_LinkedList.insert_before(9,67)
+    assert test_LinkedList.__str__() == "{ 68 } -> { 34 } -> { 1 } -> { 6 } -> { 9 } -> { 67 } -> NULL"
