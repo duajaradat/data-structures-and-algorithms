e90a5710f8b11e42e46477af1c892dd08965bf6b
duajaradat <duajaradat164@gmail.com>
2021-10-19 09:56:00 +0300

    test: test insert-includes methods

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -1,5 +1,9 @@
 from linked_list.linked_list import LinkedList
 
+def test_LinkedList_insert():
+    test_LinkedList = LinkedList()
+    test_LinkedList.insert(0)
+    test_LinkedList.insert(1)
+    assert test_LinkedList.includes(100) == False
 
-def test_import():
-    assert LinkedList
+
