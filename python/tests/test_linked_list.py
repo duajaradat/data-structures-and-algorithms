7715451d0846071ea02f79b8ee3760c4706e8d82
duajaradat <duajaradat164@gmail.com>
2021-10-19 14:23:06 +0300

    test insert after

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -35,3 +35,10 @@ def test_LinkedList_insertafter():
     test_LinkedList.append(9)
     test_LinkedList.insert_after(10,67)
     assert str(test_LinkedList) == f"{ 68 } -> { 34 } -> { 6 } -> { 9 } -> NULL"
+
+def test_LinkedList_insertafter_two():
+    test_LinkedList=LinkedList()
+    test_LinkedList.insert(6)
+    test_LinkedList.append(9)
+    test_LinkedList.insert_after(6,67)
+    assert str(test_LinkedList) == f"{ 6 } -> { 67 } -> { 9 } -> NULL"
