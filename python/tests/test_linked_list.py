3481e6e52ec57598cd725e081240416ea2a0ca77
duajaradat <duajaradat164@gmail.com>
2021-10-19 10:00:14 +0300

    test : test str method

--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -6,4 +6,9 @@ def test_LinkedList_insert():
     test_LinkedList.insert(1)
     assert test_LinkedList.includes(100) == False
 
+def test_LinkedList_str():
+    test_LinkedList = LinkedList()
+    test_LinkedList.insert(56)
+    test_LinkedList.insert(70)
+    assert test_LinkedList.__str__() == f"{ 70 } -> { 56 } -> NULL"
 
