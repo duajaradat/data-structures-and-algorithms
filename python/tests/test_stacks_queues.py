ed9ce1946cc53a72c61b765a1861d423d41bea19
duajaradat <duajaradat164@gmail.com>
2021-10-23 18:39:37 +0300

    test : test stack pop method

--- python/tests/test_stacks_queues.py
+++ python/tests/test_stacks_queues.py
@@ -5,3 +5,9 @@ def test_stack_push():
     stack.push(1)
     stack.push(2)
     assert stack.peek()==2
+
+def test_stack_pop():
+    stack=Stack()
+    stack.push(1)
+    stack.push(2)
+    assert stack.pop()==2
