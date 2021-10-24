a975ad6057e2321b911f5de3a9031829b387c0f0
duajaradat <duajaradat164@gmail.com>
2021-10-24 21:12:19 +0300

    build : build Node class , Stack class

---
+++ python/data_structure/stacks_queues/stack-queue-pseudo.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next=None
+class StackEmptyException(Exception):
+    pass
+
+
+class Stack:
+
+    """
+    Create a Stack class that has a top property. It creates an empty Stack when instantiated.
+    """
+    def __init__(self):
+        self.top=None
+
+    def push(self,value):
+        """
+        push method to add new node with data at the top of stack
+        """
+        new_node = Node(value)
+        if self.top:
+            new_node.next=self.top
+        self.top=new_node
+
+    def pop(self):
+        """
+        pop method to remove node from the top of the stack
+        """
+        if not self.top:
+            raise StackEmptyException("Empty Stack")
+        else:
+            temp=self.top
+            self.top=self.top.next
+            temp.next=None
+            return temp.value
+
+    def peek(self):
+        """
+        peek method returns the node value at the top of stack.
+        """
+        if self.isEmpty():
+            raise StackEmptyException("Empty Stack")
+        else:
+            return self.top.value
+
+    def isEmpty(self):
+        """
+        isEmpty method to check if the stack is empty or not.
+        """
+        if not self.top:
+            return True
+        else:
+            return False
