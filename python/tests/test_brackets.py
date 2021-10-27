6348b9fe5e9fa951d611144d49b405c48e688eb3
duajaradat <duajaradat164@gmail.com>
2021-10-27 06:25:34 +0300

    test : test another string contains more than one brackets

--- python/tests/test_brackets.py
+++ python/tests/test_brackets.py
@@ -3,3 +3,6 @@ from code_challenges.brackets.brackets import validate_brackets
 
 def test_one():
     assert validate_brackets('(')==False
+
+def test_two():
+    assert validate_brackets('[({\}]')==False
