49eebf0312236ecc86267e36563b9eca9fb1db52
duajaradat <duajaradat164@gmail.com>
2021-10-27 06:17:48 +0300

    edit the code

--- python/code_challenges/brackets/brackets.py
+++ python/code_challenges/brackets/brackets.py
@@ -1,26 +1,33 @@
-from data_structure.stacks_queues .stacks_queues import Stack
-def validate_brackets(string):
+
+def validate_brackets(input):
 
     """
     Arguments: string
     Return: boolean
          representing whether or not the brackets in the string are balanced
     """
-    stack=Stack()
+    stack=[]
     brackets={
         '[':']',
         '(':')',
         '{':'}',
     }
 
-    for char in string:
+    for char in input:
         if char in brackets:
-            stack.push(char)
+            stack.append(char)
         if char in brackets.values():
-            if char == stack.peek():
+            if char == brackets[stack[-1]]:
                 stack.pop()
 
-    if stack.isEmpty():
+    if len(stack)==0:
         return True
     else:
         return False
+
+
+if __name__ == '__main__':
+    input = '{\}{Code}[Fellows](())'
+    print(validate_brackets(input))
+    input2='(]('
+    print(validate_brackets(input2))
