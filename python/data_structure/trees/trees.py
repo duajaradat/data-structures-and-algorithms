fd39869e15cf4b6fc106e37a98942137ddb0a9f4
duajaradat <duajaradat164@gmail.com>
2021-10-30 21:58:35 +0200

    build : build Node class , Queue Class and BinaryTree class with breadth method

---
+++ python/data_structure/trees/trees.py
@@ -0,0 +1,48 @@
+class Node:
+    """
+    Node class that has properties for the value stored in the node
+    """
+    def __init__(self,data,left=None,right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+class Queue:
+    def __init__(self,collection=[]):
+        self.collection = collection
+
+    def enqueue(self,value):
+        self.collection.append(value)
+
+    def dequeue(self):
+        return self.collection.pop(0)
+
+    def peek(self):
+        if len(self.collection):
+            return  True
+        return False
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def breadth(self):
+        """
+        breadth first search method that returns list of values that Contains
+        input -> None
+        return -> list
+        """
+        dfs_output = []
+        dfs =Queue()
+
+        dfs.enqueue(self.root)
+
+        while dfs.peek():
+            front=dfs.dequeue()
+            dfs_output+=[front.data]
+
+            if front.left :
+                dfs.enqueue(front.left)
+            if front.right :
+                dfs.enqueue(front.right)
+        return dfs_output
